6403cf72b0c8300c86a20fd6903ecfaeae39a50b
Bhavan Sai <91448593+BhavanSai12@users.noreply.github.com>
2023-02-14 18:48:11 +0530

    MoneyPlatform Application

---
+++ src/main/java/com/bhavan/moneyApp/Portfoilo/PortfoiloApplication.java
@@ -0,0 +1,13 @@
+package com.bhavan.moneyApp.Portfoilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhavan/moneyApp/Portfoilo/PortfoiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavan.moneyApp.Portfoilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
