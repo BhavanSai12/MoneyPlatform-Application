97efd4938657020938db50dabe8b283bf99e0273
Bhavan Sai <91448593+BhavanSai12@users.noreply.github.com>
2023-02-14 20:52:22 +0530

    updated

--- src/main/java/com/bhavan/moneyApp/Portfoilo/entity/Rebalancing.java
+++ src/main/java/com/bhavan/moneyApp/Portfoilo/entity/Rebalancing.java
@@ -1,4 +1,75 @@
 package com.bhavan.moneyApp.Portfoilo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Rebalancing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long rebalancingId;
+    private int rebalancedValue;
+    private int rebalancedPercentage;
+    private int allocationPercentage;
+    private int portfolioId;
+    private int portfolioValue;
+
+    public Rebalancing(long rebalancingId, int rebalancedValue, int rebalancedPercentage, int allocationPercentage, int portfolioId, int portfolioValue) {
+        this.rebalancingId = rebalancingId;
+        this.rebalancedValue = rebalancedValue;
+        this.rebalancedPercentage = rebalancedPercentage;
+        this.allocationPercentage = allocationPercentage;
+        this.portfolioId = portfolioId;
+        this.portfolioValue = portfolioValue;
+    }
+
+    public long getRebalancingId() {
+        return rebalancingId;
+    }
+
+    public void setRebalancingId(long rebalancingId) {
+        this.rebalancingId = rebalancingId;
+    }
+
+    public int getRebalancedValue() {
+        return rebalancedValue;
+    }
+
+    public void setRebalancedValue(int rebalancedValue) {
+        this.rebalancedValue = rebalancedValue;
+    }
+
+    public int getRebalancedPercentage() {
+        return rebalancedPercentage;
+    }
+
+    public void setRebalancedPercentage(int rebalancedPercentage) {
+        this.rebalancedPercentage = rebalancedPercentage;
+    }
+
+    public int getAllocationPercentage() {
+        return allocationPercentage;
+    }
+
+    public void setAllocationPercentage(int allocationPercentage) {
+        this.allocationPercentage = allocationPercentage;
+    }
+
+    public int getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(int portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+    public int getPortfolioValue() {
+        return portfolioValue;
+    }
+
+    public void setPortfolioValue(int portfolioValue) {
+        this.portfolioValue = portfolioValue;
+    }
 }
---
+++ src/main/java/com/bhavan/moneyApp/Portfoilo/exception/ResourceNotFoundException.java
@@ -0,0 +1,23 @@
+package com.bhavan.moneyApp.Portfoilo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+/**
+ * Annotation @ResponseStatus sets the HTTP status code for the response. In
+ * this case, the HTTP status code is set to 404 (NOT_FOUND).
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    private String message;
+    private static final long serialVersionUID=1L;
+
+    /**
+     * Constructor to create a ResourceNotFoundException instance
+     *
+     * @param message the error message for this exception
+     */
+    public ResourceNotFoundException(String message){
+        super(message);
+        this.message=message;
+    }
+}
